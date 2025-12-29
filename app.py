ba8bda29f8240ff577dc39b60e969cfa732d1093
yosefeitn100mars <yosefeitn100@gmail.com>
2025-12-29 22:01:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -22,52 +22,39 @@ def process_comic(image_bytes):
     pil_img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
     draw = ImageDraw.Draw(pil_img)
     
-    # טעינת הפונט - גודל 13 כדי שיהיה קטן ובטוח
+    # שימוש בשם החדש כדי לעקוף את הזיכרון של Streamlit
     try:
-        font = ImageFont.truetype("font.ttf", 13)
+        font = ImageFont.truetype("newfont.ttf", 14)
     except:
         font = ImageFont.load_default()
 
     for (bbox, text, prob) in results:
         if prob > 0.2:
-            # זיהוי מיקום וגודל
             top_left = tuple(map(int, bbox[0]))
             bottom_right = tuple(map(int, bbox[2]))
             x, y = top_left
             w = bottom_right[0] - x
             h = bottom_right[1] - y
             
-            # 1. מחיקה חזקה - אנחנו יוצרים מלבן לבן קצת יותר גדול מהטקסט
-            draw.rectangle([x-4, y-4, x+w+4, y+h+4], fill="white")
+            # כיסוי לבן רחב יותר כדי למחוק את האנגלית לגמרי
+            draw.rectangle([x-5, y-5, x+w+5, y+h+5], fill="white")
             
             try:
-                # 2. תרגום
                 translated = translator.translate(text)
-                
-                # 3. טיפול בטקסט ארוך - חלוקה לשורות
-                words = translated.split()
-                if len(words) > 3:
-                    mid = len(words) // 2
-                    line1 = " ".join(words[:mid])[::-1]
-                    line2 = " ".join(words[mid:])[::-1]
-                    display_text = line1 + "\n" + line2
-                else:
-                    display_text = translated[::-1]
-                
-                # 4. ציור במרכז
-                draw.multiline_text((x + w/2, y + h/2), display_text, 
-                                  fill="black", font=font, anchor="mm", 
-                                  align="center", spacing=2)
+                # היפוך אותיות לעברית
+                display_text = translated[::-1]
+                # כתיבה במרכז הבועה
+                draw.text((x + w/2, y + h/2), display_text, fill="black", font=font, anchor="mm")
             except:
                 pass
     return pil_img
 
-st.title("מתרגם קומיקס")
-file = st.file_uploader("העלה דף קומיקס", type=["jpg", "png", "jpeg"])
+st.title("מתרגם קומיקס - גרסת תיקון פונט")
+file = st.file_uploader("העלה תמונה", type=["jpg", "png", "jpeg"])
 
 if file:
     if st.button("תרגם עכשיו"):
-        with st.spinner("מנקה אנגלית וכותב עברית..."):
+        with st.spinner("מנקה זיכרון ומתרגם..."):
             file.seek(0)
             res = process_comic(file.read())
             st.image(res, use_container_width=True)
