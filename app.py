04c70cbbd0551b91043079580c68171821a22b35
yosefeitn100mars <yosefeitn100@gmail.com>
2025-12-29 21:44:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from PIL import Image, ImageDraw, ImageFont
 from deep_translator import GoogleTranslator
 import easyocr
 
-st.set_page_config(page_title="Comic Translator")
+st.set_page_config(page_title="Comic Translator Pro")
 
 @st.cache_resource
 def load_reader():
@@ -20,46 +20,4 @@ def process_comic(image_bytes):
     results = reader.readtext(img)
     
     pil_img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-    draw = ImageDraw.Draw(pil_img)
-    
-    # טעינת פונט בגודל קטן יותר (14 במקום 18) כדי שיכנס בבטחה
-    try:
-        font = ImageFont.truetype("font.ttf", 14)
-    except:
-        font = ImageFont.load_default()
-
-    for (bbox, text, prob) in results:
-        if prob > 0.2:
-            # הגדרת אזור הבועה
-            top_left = tuple(map(int, bbox[0]))
-            bottom_right = tuple(map(int, bbox[2]))
-            x, y = top_left
-            w = bottom_right[0] - x
-            h = bottom_right[1] - y
-            
-            # 1. מחיקה מוחלטת של האנגלית (מלבן לבן נקי)
-            # הוספנו "ביטחון" של 3 פיקסלים לכל כיוון
-            draw.rectangle([x-3, y-3, x+w+3, y+h+3], fill="white")
-            
-            try:
-                # 2. תרגום
-                translated = translator.translate(text)
-                
-                # 3. הכנת הטקסט - רק היפוך בסיסי (בלי ניקוי תווים מסובך)
-                display_text = translated[::-1]
-                
-                # 4. כתיבה במרכז הבועה - טקסט קטן ושחור
-                draw.text((x + w/2, y + h/2), display_text, fill="black", font=font, anchor="mm")
-            except:
-                pass
-    return pil_img
-
-st.title("מתרגם קומיקס - גרסה פשוטה")
-file = st.file_uploader("העלה תמונה", type=["jpg", "png", "jpeg"])
-
-if file:
-    if st.button("תרגם עכשיו"):
-        with st.spinner("מנקה וכותב בעברית..."):
-            file.seek(0)
-            res = process_comic(file.read())
-            st.image(res, use_container_width=True)
+    draw
