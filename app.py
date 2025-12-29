ea5b836f3d6f4077bb40323b9a20ff4ff5a27a31
yosefeitn100mars <yosefeitn100@gmail.com>
2025-12-29 21:18:12 +0200

    Enhance comic translator with new features

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import cv2
 import numpy as np
-from PIL import Image, ImageDraw, ImageFont # חשוב לייבא את ImageFont
+from PIL import Image, ImageDraw, ImageFont
 from deep_translator import GoogleTranslator
 import easyocr
+import textwrap
 
-st.set_page_config(page_title="Comic Translator")
+st.set_page_config(page_title="Comic Translator Pro")
 
 @st.cache_resource
 def load_reader():
@@ -22,12 +23,12 @@ def process_comic(image_bytes):
     pil_img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
     draw = ImageDraw.Draw(pil_img)
     
-    # טעינת הפונט שהעלית - וודא שהשם כאן זהה בדיוק לשם הקובץ ב-GitHub!
     try:
-        # אם קראת לקובץ בשם אחר (למשל font.otf), שנה כאן
-        font = ImageFont.truetype("font.ttf", 20) 
+        font_path = "font.otf" 
+        # פונט בסיסי לחישובים
+        base_font = ImageFont.truetype(font_path, 16)
     except:
-        font = ImageFont.load_default()
+        base_font = ImageFont.load_default()
 
     for (bbox, text, prob) in results:
         if prob > 0.2:
@@ -35,25 +36,38 @@ def process_comic(image_bytes):
             x, y = int(top_left[0]), int(top_left[1])
             w, h = int(bottom_right[0] - x), int(bottom_right[1] - y)
             
-            # ניקוי הבועה (צביעה בלבן)
-            draw.rectangle([x-2, y-2, x+w+2, y+h+2], fill="white")
+            # ניקוי הבועה
+            draw.rectangle([x, y, x+w, y+h], fill="white")
             
             try:
                 translated = translator.translate(text)
-                # תיקון היפוך לעברית (חשוב!)
-                display_text = translated[::-1] 
-                # כתיבת הטקסט עם הפונט החדש
-                draw.text((x + w//2, y + h//2), display_text, fill="black", font=font, anchor="mm")
+                
+                # שבירת שורות אוטומטית לפי רוחב הבועה
+                # נחשב כמה תווים נכנסים ברוחב (בערך)
+                chars_per_line = max(1, w // 8) 
+                lines = textwrap.wrap(translated, width=chars_per_line)
+                
+                # היפוך כל שורה בנפרד וחיבורן מחדש עם ירידת שורה
+                display_text = "\n".join([line[::-1] for line in lines])
+                
+                # התאמת גודל הפונט לכמות הטקסט
+                current_font_size = 18
+                if len(lines) > 2: current_font_size = 14
+                font = ImageFont.truetype(font_path, current_font_size)
+                
+                # ציור הטקסט במרכז הבועה
+                draw.multiline_text((x + w//2, y + h//2), display_text, fill="black", 
+                                  font=font, anchor="mm", align="center")
             except:
                 pass
 
     return pil_img
 
-st.title("🎨 מתרגם קומיקס")
-file = st.file_uploader("העלה תמונה", type=["jpg", "png", "jpeg"])
+st.title("🎨 מתרגם קומיקס מקצועי")
+file = st.file_uploader("העלה עמוד קומיקס", type=["jpg", "png", "jpeg"])
 
 if file:
     if st.button("תרגם עכשיו"):
-        with st.spinner("הבינה המלאכותית קוראת ומתרגמת..."):
-            result = process_comic(file.read())
-            st.image(result, use_container_width=True)
+        with st.spinner("מנתח טקסט ומסדר שורות..."):
+            res = process_comic(file.read())
+            st.image(res, use_container_width=True)
