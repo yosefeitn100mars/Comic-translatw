1653b0ab29afd783c909854032f01a473928bd80
yosefeitn100mars <yosefeitn100@gmail.com>
2025-12-29 22:12:32 +0200

    Refactor comic translator for Hebrew support
    Updated the app title and improved text processing for Hebrew translation. Adjusted font loading and sensitivity for text detection.

--- app.py
+++ app.py
@@ -5,11 +5,18 @@ from PIL import Image, ImageDraw, ImageFont
 from deep_translator import GoogleTranslator
 import easyocr
 
-st.set_page_config(page_title="Final Fix")
+st.set_page_config(page_title="Comic Translator Pro")
 
 @st.cache_resource
 def load_reader():
+    # טעינת המודל לאנגלית
     return easyocr.Reader(['en'])
+
+def reverse_hebrew_logic(text):
+    # הופך כל מילה בנפרד ואז את סדר המילים כדי שהמשפט יהיה קריא
+    words = text.split()
+    reversed_words = [word[::-1] for word in words]
+    return " ".join(reversed_words[::-1])
 
 def process_comic(image_bytes):
     reader = load_reader()
@@ -22,38 +29,48 @@ def process_comic(image_bytes):
     pil_img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
     draw = ImageDraw.Draw(pil_img)
     
-    # השם החדש כדי לעקוף את הזיכרון של המערכת
     try:
-        font = ImageFont.truetype("my_new_font_v1.ttf", 15)
+        font = ImageFont.truetype("font.ttf", 16)
     except:
         font = ImageFont.load_default()
 
     for (bbox, text, prob) in results:
-        if prob > 0.15: # רגישות גבוהה יותר לזיהוי
+        if prob > 0.2:
             top_left = tuple(map(int, bbox[0]))
             bottom_right = tuple(map(int, bbox[2]))
             x, y = top_left
             w, h = bottom_right[0] - x, bottom_right[1] - y
             
-            # מחיקה רחבה מאוד של האנגלית
-            draw.rectangle([x-5, y-5, x+w+5, y+h+5], fill="white")
+            # ניקוי הבועה עם מלבן לבן נקי
+            draw.rectangle([x-2, y-2, x+w+2, y+h+2], fill="white")
             
             try:
                 translated = translator.translate(text)
-                # היפוך אותיות ידני לעברית
-                display_text = translated[::-1]
                 
-                # כתיבה במרכז
-                draw.text((x + w/2, y + h/2), display_text, fill="black", font=font, anchor="mm")
+                # סידור המשפט לעברית תקנית
+                display_text = reverse_hebrew_logic(translated)
+                
+                # חלוקה לשורות אם הטקסט ארוך מהבועה
+                if len(display_text) > 15:
+                    mid = len(display_text) // 2
+                    split_idx = display_text.find(' ', mid - 5, mid + 5)
+                    if split_idx != -1:
+                        display_text = display_text[:split_idx] + "\n" + display_text[split_idx+1:]
+
+                # כתיבה במרכז הבועה
+                draw.multiline_text((x + w/2, y + h/2), display_text, 
+                                  fill="black", font=font, anchor="mm", 
+                                  align="center", spacing=4)
             except:
                 pass
     return pil_img
 
-st.title("מתרגם קומיקס - גרסה סופית")
-file = st.file_uploader("העלה תמונה", type=["jpg", "png", "jpeg"])
+st.title("🎨 מתרגם הקומיקס שלי - מוכן!")
+file = st.file_uploader("העלה דף קומיקס", type=["jpg", "png", "jpeg"])
 
 if file:
     if st.button("תרגם עכשיו"):
-        file.seek(0)
-        res = process_comic(file.read())
-        st.image(res, use_container_width=True)
+        with st.spinner("יוצר תוצאה מושלמת..."):
+            file.seek(0)
+            res = process_comic(file.read())
+            st.image(res, use_container_width=True)
